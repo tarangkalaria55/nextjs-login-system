a6c5ff1ee6dfacce096f283bf8d4b45278e5c1ee
Tarang Kalariya <tarangkalaria55@gmail.com>
2025-10-09 22:28:20 +0530

    middleware setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { headers } from "next/headers";
+import { type NextRequest, NextResponse } from "next/server";
+import { auth } from "./lib/auth/auth";
+import { createRouteMatcher } from "./lib/clerkjs/routeMatcher";
+
+const isAuthRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
+
+const isPublicRoute = createRouteMatcher([]);
+
+const loginUrl = "/sign-in";
+const dashboardUrl = "/dashboard";
+
+export async function middleware(request: NextRequest) {
+  const isPublicUrl = isPublicRoute(request);
+  const isAuthUrl = isAuthRoute(request);
+  const isProtectedUrl = !isPublicUrl && !isAuthUrl;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const isLoggedIn = !!session;
+
+  if (isAuthUrl && isLoggedIn) {
+    return NextResponse.redirect(new URL(dashboardUrl, request.url));
+  }
+
+  if (isProtectedUrl && !isLoggedIn) {
+    return NextResponse.redirect(new URL(loginUrl, request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  runtime: "nodejs",
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
